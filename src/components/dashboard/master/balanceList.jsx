4e1a4382c8e245f3a70f32b956eb814c3390b7e4
Tjipto Steven Senjaya <steveneonites@gmail.com>
2022-11-03 18:13:36 +0700

    coba trial balance

--- src/components/dashboard/master/balanceList.jsx
+++ src/components/dashboard/master/balanceList.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react'
-import Modal from '../../site/modal'
+import React, { useState, useEffect } from "react";
+import Modal from "../../site/modal";
 
 const mapChild = (list) => {
-  let arr = list
-  let c = list.credit
-  let d = list.debit
-  let x = 0
+  let arr = list;
+  let c = list.credit;
+  let d = list.debit;
+  let x = 0;
   try {
     if (arr.child > 0) {
       function mapEach(arr) {
@@ -13,176 +13,270 @@ const mapChild = (list) => {
           <>
             {e.debit - e.credit}
             <br />
-            {e.child > 0 ? mapEach(e.child) : ''}
+            {e.child > 0 ? mapEach(e.child) : ""}
           </>
-        ))
+        ));
       }
-      mapEach(arr)
+      return mapEach(arr);
     }
     // x = d - c
     // return x
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 const checkChild = (list) => {
-  let arr = list
-  let c = list.credit
-  let d = list.debit
-  let x = 0
+  let arr = list;
+  let c = list.credit;
+  let d = list.debit;
+  let x = 0;
   try {
     if (arr.child > 0) {
       function childEach(arr) {
         arr.child.forEach((e) => {
-          c += e.credit
-          d += e.debit
+          c += e.credit;
+          d += e.debit;
           if (e.child > 0) {
-            childEach(e.child)
+            childEach(e.child);
           }
-        })
+        });
       }
-      childEach(arr)
+      childEach(arr);
     }
-    x = d - c
-    return x
+    x = d - c;
+    return x;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 const grandTotal = (list) => {
   try {
-    let y = ''
-    let x = 0
+    let y = "";
+    let x = 0;
     if (list.child.length > 0) {
-      y = list.name
-      let z = 0
+      y = list.name;
+      let z = 0;
       let deb,
-        cred = 0
-      deb = parseFloat(list.debit)
-      cred = parseFloat(list.credit)
+        cred = 0;
+      deb = parseFloat(list.debit);
+      cred = parseFloat(list.credit);
       list.child.forEach((e) => {
-        if (e.type === 'Assets' || e.type === 'Expense') {
-          x = x + (parseFloat(e.debit) + deb - (cred + parseFloat(e.credit)))
+        if (e.type === "Assets" || e.type === "Expense") {
+          x = x + (parseFloat(e.debit) + deb - (cred + parseFloat(e.credit)));
         } else {
-          x = x + (cred + parseFloat(e.credit) - (parseFloat(e.debit) + deb))
+          x = x + (cred + parseFloat(e.credit) - (parseFloat(e.debit) + deb));
         }
-      })
+      });
     }
     // console.log(y, x)
 
-    return `${x}.00`
+    return `${x}.00`;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 function BalanceList({ list, btn }) {
   // const list = coaTotal(lists)
-  const [data, setData] = useState({ vis: false, toggle: false })
-  const [vis, setVis] = useState({ modal: false })
+  const [data, setData] = useState({ vis: false, toggle: false });
+  const [vis, setVis] = useState({ modal: false });
   const nestedCoa = (list.child || []).map((d) => {
-    return <BalanceList key={d.number} list={d} type="child" btn={btn} />
-  })
+    return <BalanceList key={d.number} list={d} type="child" btn={btn} />;
+  });
+  const nestTotal = (list) => {
+    let c = parseFloat(list.credit);
+    let d = parseFloat(list.debit);
+    let t = 0;
+    const childTotal = (li) => {
+      (li.child || []).map((e) =>
+        e.type === "Assets" || e.type === "Expense"
+          ? (t = d + parseFloat(e.debit) - (c + parseFloat(e.credit)))
+          : (t = c + parseFloat(e.credit) - (d + parseFloat(e.debit)))
+      );
+    };
+    if (list.child) {
+      childTotal(list);
+    }
+
+    return t;
+  };
+  const nestedTotal = (list.child || []).map((e) => {
+    let c = [];
+    let d = [];
+    let t = 0;
+    let a = [];
+    return (
+      <>
+        {e.type === "Assets" || e.type === "Expense"
+          ? (t = d + parseFloat(e.debit) - (c + parseFloat(e.credit)))
+          : (t = c + parseFloat(e.credit) - (d + parseFloat(e.debit)))}
+        {(e.child || []).map((f) => {
+          return (
+            <>
+              {f.type === "Assets" || f.type === "Expense"
+                ? f.debit - f.credit
+                : f.credit - f.debit}
+              {(f.child || []).map((g) => {
+                return (
+                  <>
+                    {g.type === "Assets" || g.type === "Expense"
+                      ? g.debit - g.credit
+                      : g.credit - g.debit}
+
+                    {(g.child || []).map((h) => {
+                      return (
+                        <>
+                          {h.type === "Assets" || h.type === "Expense"
+                            ? h.debit - h.credit
+                            : h.credit - h.debit}
+
+                          {(h.child || []).map((i) => {
+                            return (
+                              <>
+                                {i.type === "Assets" || i.type === "Expense"
+                                  ? i.debit - i.credit
+                                  : i.credit - i.debit}
+
+                                {(i.child || []).map((j) => {
+                                  return (
+                                    <>
+                                      {j.type === "Assets" ||
+                                      j.type === "Expense"
+                                        ? j.debit - j.credit
+                                        : j.credit - j.debit}
+
+                                      {(j.child || []).map((k) => {
+                                        return (
+                                          <>
+                                            {k.type === "Assets" ||
+                                            k.type === "Expense"
+                                              ? k.debit - k.credit
+                                              : k.credit - k.debit}
+                                          </>
+                                        );
+                                      })}
+                                    </>
+                                  );
+                                })}
+                              </>
+                            );
+                          })}
+                        </>
+                      );
+                    })}
+                  </>
+                );
+              })}
+            </>
+          );
+        })}
+      </>
+    );
+  });
 
   const handleClose = (e) => {
-    setVis({ ...vis, modal: false })
-  }
+    setVis({ ...vis, modal: false });
+  };
   const handleAddChild = (e) => {
-    setVis({ ...vis, modal: true, value: 1 })
-  }
+    setVis({ ...vis, modal: true, value: 1 });
+  };
   const handleEdit = (e) => {
-    setVis({ ...vis, modal: true, value: 2 })
-  }
+    setVis({ ...vis, modal: true, value: 2 });
+  };
   const handleDelete = (e) => {
-    setVis({ ...vis, modal: true, value: 3 })
-  }
+    setVis({ ...vis, modal: true, value: 3 });
+  };
 
   return (
     <>
-      {console.log(list)}
+      {/* {console.log(list)} */}
 
       <div
-        style={{ paddingLeft: '20px', marginTop: '5px' }}
+        style={{ paddingLeft: "20px", marginTop: "5px" }}
         onMouseOver={() => setData({ ...data, toggle: true })}
         onMouseLeave={() => setData({ ...data, toggle: false })}
       >
         <div
           style={{
-            display: 'flex',
-            justifyContent: 'space-around',
-            margin: '0 0 2px 0',
+            display: "flex",
+            justifyContent: "space-around",
+            margin: "0 0 2px 0",
           }}
         >
           <div
             style={{
-              display: 'flex',
-              justifyContent: 'flex-start',
-              width: '45%',
+              display: "flex",
+              justifyContent: "flex-start",
+              width: "45%",
             }}
           >
-            {list.is_group === '1' ? (
-              <i className="bi bi-folder" style={{ marginRight: '10px' }}></i>
+            {list.is_group === "1" ? (
+              <i className="bi bi-folder" style={{ marginRight: "10px" }}></i>
             ) : (
-              <i className="bi bi-file" style={{ marginRight: '10px' }}></i>
+              <i className="bi bi-file" style={{ marginRight: "10px" }}></i>
             )}
-            <div style={{ color: 'white' }}>
+            <div style={{ color: "white" }}>
               {list.number} - {list.name}
             </div>
           </div>
           <div
             className="col-md-6"
             style={{
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'flex-end',
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "flex-end",
             }}
           >
             <div
               style={{
-                display: 'flex',
-                justifyContent: 'flex-end',
-                width: '45%',
-                color: 'white',
+                display: "flex",
+                justifyContent: "flex-end",
+                width: "45%",
+                color: "white",
               }}
             >
-              {list.debit === '0.00'
-                ? '-'
-                : list.debit.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}
+              {list.debit === "0.00"
+                ? "-"
+                : list.debit.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")}
             </div>
             <div
               style={{
-                display: 'flex',
-                justifyContent: 'flex-end',
-                width: '45%',
-                color: 'white',
+                display: "flex",
+                justifyContent: "flex-end",
+                width: "45%",
+                color: "white",
               }}
             >
-              {list.credit === '0.00'
-                ? '-'
-                : list.credit.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}
+              {list.credit === "0.00"
+                ? "-"
+                : list.credit.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")}
             </div>
             <div
               style={{
-                display: 'flex',
-                justifyContent: 'flex-end',
-                width: '45%',
-                color: 'white',
+                display: "flex",
+                justifyContent: "flex-end",
+                width: "45%",
+                color: "white",
               }}
             >
-              {mapChild(list)} |
-              {grandTotal(list).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}
+              {JSON.stringify(list)}
+              {nestedTotal}
+              <br />
+              <br />
+              {/* {mapChild(list)} | */}
+              {/* {grandTotal(list).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")} */}
               {/* {list.total === '0.00' ? '-' : list.total} */}
             </div>
           </div>
         </div>
         {nestedCoa}
       </div>
-      {list.parent === '0' && (
-        <div className="w-100" style={{ height: '15px' }}></div>
+      {list.parent === "0" && (
+        <div className="w-100" style={{ height: "15px" }}></div>
       )}
     </>
-  )
+  );
 }
-export default BalanceList
+export default BalanceList;
--- src/components/report/trialBalance.jsx
+++ src/components/report/trialBalance.jsx
@@ -1,30 +1,32 @@
-import React, { useMemo, useState } from 'react'
-import { useQuery } from 'react-query'
-import createTree from '../custom/createTree'
-import BalanceLists from '../dashboard/master/balanceLists'
-import { reqCoa, reqCoaList, reqJournalEntry, reqPeriod } from '../reqFetch'
-import useFetch from '../useFetch'
-import ReportList from './reportList'
-import ReportTable from './reportTable'
+import React, { useMemo, useState } from "react";
+import { useQuery } from "react-query";
+import createTree from "../custom/createTree";
+import BalanceLists from "../dashboard/master/balanceLists";
+import { reqCoa, reqCoaList, reqJournalEntry, reqPeriod } from "../reqFetch";
+import useFetch from "../useFetch";
+import ReportList from "./reportList";
+import ReportTable from "./reportTable";
 
 const TrialBalance = () => {
   // const { data: trial } = useFetch('gettrial.php')
-  const [data, setData] = useState({ period: '' })
-  const [vis, setVis] = useState({ modal: false })
-  let periodStorage = localStorage.getItem('period')
+  const [data, setData] = useState({ period: "" });
+  const [vis, setVis] = useState({ modal: false });
+  let periodStorage = localStorage.getItem("period");
   // let period = JSON.parse(periodStorage);
   // const { data: coaList } = useFetch('getcoalist.php')
 
-  const { data: period } = useQuery('period', reqPeriod)
-  const { data: coa } = useQuery('coa', reqCoa)
-  const { data: journalEntry } = useQuery('journalEntry', reqJournalEntry)
-  const { data: coaList, error, isError, isLoading } = useQuery(
-    'coaList',
-    reqCoaList,
-  )
+  const { data: period } = useQuery("period", reqPeriod);
+  const { data: coa } = useQuery("coa", reqCoa);
+  const { data: journalEntry } = useQuery("journalEntry", reqJournalEntry);
+  const {
+    data: coaList,
+    error,
+    isError,
+    isLoading,
+  } = useQuery("coaList", reqCoaList);
 
   // create a new COA
-  let newCoa = []
+  let newCoa = [];
   coaList?.forEach((e) => {
     try {
       let x = {
@@ -33,16 +35,16 @@ const TrialBalance = () => {
         type: e.type,
         parent: e.parent,
         is_group: e.is_group,
-        debit: '0.00',
-        credit: '0.00',
-        total: '0.00',
-      }
-      newCoa.push(x)
+        debit: "0.00",
+        credit: "0.00",
+        total: "0.00",
+      };
+      newCoa.push(x);
     } catch (error) {}
-  })
+  });
   // Filter journal Entry by period
   let jE = useMemo(() => {
-    return data.period === ''
+    return data.period === ""
       ? journalEntry?.sort((a, b) => (a.created_date > b.created_date ? 1 : -1))
       : journalEntry
           ?.sort((a, b) => (a.created_date > b.created_date ? 1 : -1))
@@ -51,112 +53,113 @@ const TrialBalance = () => {
               new Date(d.created_date) >=
                 new Date(period[parseInt(data.period)].start) &&
               new Date(d.created_date) <=
-                new Date(period[parseInt(data.period)].end),
-          )
-  }, [journalEntry, period, data.period])
+                new Date(period[parseInt(data.period)].end)
+          );
+  }, [journalEntry, period, data.period]);
   // new COA by filtered Journal Entry
   jE?.forEach((e) => {
-    if (e.acc !== 'Total') {
+    if (e.acc !== "Total") {
       try {
-        let i = newCoa.findIndex((d) => d.number === e.acc)
-        let d, c
+        let i = newCoa.findIndex((d) => d.number === e.acc);
+        let d, c;
         // console.log(e.acc, e.debit, parseInt(e.debit))
-        d = parseInt(e.debit) + parseInt(newCoa[i].debit)
-        c = parseInt(e.credit) + parseInt(newCoa[i].credit)
-        let t = 0
-        if (newCoa[i].type === 'Assets' || newCoa[i].type === 'Expense') {
-          t = d - c
+        d = parseInt(e.debit) + parseInt(newCoa[i].debit);
+        c = parseInt(e.credit) + parseInt(newCoa[i].credit);
+        let t = 0;
+        if (newCoa[i].type === "Assets" || newCoa[i].type === "Expense") {
+          t = d - c;
         } else {
-          t = c - d
+          t = c - d;
         }
-        let y = newCoa
+        let y = newCoa;
         let x = {
           number: newCoa[i].number,
           name: newCoa[i].name,
           type: newCoa[i].type,
           parent: newCoa[i].parent,
           is_group: newCoa[i].is_group,
-          debit: d.toString() + '.00',
-          credit: c.toString() + '.00',
-          total: t.toString() + '.00',
-        }
-        y[i] = x
-        newCoa = y
+          debit: d.toString() + ".00",
+          credit: c.toString() + ".00",
+          total: t.toString() + ".00",
+        };
+        y[i] = x;
+        newCoa = y;
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     }
-  })
-  let assets = 0
-  let liability = 0
-  let equity = 0
-  let income = 0
-  let expense = 0
+  });
+  let assets = 0;
+  let liability = 0;
+  let equity = 0;
+  let income = 0;
+  let expense = 0;
   newCoa?.forEach((element) => {
-    if (element.type === 'Liability') {
-      liability += parseFloat(element.total)
-    } else if (element.type === 'Equity') {
-      equity += parseFloat(element.total)
-    } else if (element.type === 'Income') {
-      income += parseFloat(element.total)
+    if (element.type === "Liability") {
+      liability += parseFloat(element.total);
+    } else if (element.type === "Equity") {
+      equity += parseFloat(element.total);
+    } else if (element.type === "Income") {
+      income += parseFloat(element.total);
     }
-  })
+  });
   newCoa?.forEach((element) => {
-    if (element.type === 'Assets') {
-      assets += parseFloat(element.total)
-    } else if (element.type === 'Expense') {
-      expense += parseFloat(element.total)
+    if (element.type === "Assets") {
+      assets += parseFloat(element.total);
+    } else if (element.type === "Expense") {
+      expense += parseFloat(element.total);
     }
-  })
+  });
 
   let assetsFill = useMemo(() => {
     return (
       newCoa &&
       newCoa
         // .sort((a, b) => (a.name > b.name ? 1 : -1))
-        .filter((d) => d.type === 'Assets')
-    )
-  }, [newCoa])
+        .filter((d) => d.type === "Assets")
+    );
+  }, [newCoa]);
   let liabilityFill = useMemo(() => {
     return (
       newCoa &&
       newCoa
         // .sort((a, b) => (a.name > b.name ? 1 : -1))
-        .filter((d) => d.type === 'Liability')
-    )
-  }, [newCoa])
+        .filter((d) => d.type === "Liability")
+    );
+  }, [newCoa]);
   let equityFill = useMemo(() => {
     return (
       newCoa &&
       newCoa
         // .sort((a, b) => (a.name > b.name ? 1 : -1))
-        .filter((d) => d.type === 'Equity')
-    )
-  }, [newCoa])
+        .filter((d) => d.type === "Equity")
+    );
+  }, [newCoa]);
   const handleClose = (e) => {
-    setData({ ...data, vis: false })
-  }
+    setData({ ...data, vis: false });
+  };
   const handleChange = (e) => {
-    console.log(`${[e.target.name]}`, e.target.value)
+    console.log(`${[e.target.name]}`, e.target.value);
     setData({
       ...data,
       [e.target.name]: e.target.value,
-    })
-  }
-  const coba = createTree(newCoa)
+    });
+  };
+  const coba = createTree(newCoa);
   if (isLoading) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
   if (isError) {
-    return <div>Error! {error.message}</div>
+    return <div>Error! {error.message}</div>;
   }
   return (
     <>
-      {console.log(newCoa)}
+      {/* {console.log(newCoa)} */}
+      {/* {JSON.stringify(assetsFill)} */}
       {/* Component Title */}
       <div
         className="w-100"
-        style={{ display: 'flex', justifyContent: 'space-between' }}
+        style={{ display: "flex", justifyContent: "space-between" }}
       >
         <div className=" __content_title"> Adj. Trial Balance</div>
         {/* add User + search */}
@@ -178,7 +181,7 @@ const TrialBalance = () => {
             name="period"
             onChange={handleChange}
             id="period"
-            style={{ minWidth: '100px' }}
+            style={{ minWidth: "100px" }}
           >
             <option value="">Period</option>
             {period?.map((d, i) => (
@@ -188,39 +191,39 @@ const TrialBalance = () => {
           <button
             className="btn btn-primary m-1"
             onClick={() => window.print()}
-            style={{ minWidth: 'fit-content' }}
+            style={{ minWidth: "fit-content" }}
           >
             <i className="bi bi-arrow-right-square"></i>
           </button>
           <button
             className="btn btn-primary m-1"
             onClick={() => window.print()}
-            style={{ minWidth: 'fit-content' }}
+            style={{ minWidth: "fit-content" }}
           >
             <i className="bi bi-printer"></i>
           </button>
         </div>
       </div>
-      <hr style={{ margin: '0' }} />
-      <div className="w-100" style={{ height: '25px' }}></div>
+      <hr style={{ margin: "0" }} />
+      <div className="w-100" style={{ height: "25px" }}></div>
       <div
         style={{
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'center',
-          padding: '7px 20px 7px 40px',
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "7px 20px 7px 40px",
         }}
       >
         <div className="col-md-6">Account</div>
         <div
           className="col-md-6"
           style={{
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'center',
-            textAlign: 'right',
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            textAlign: "right",
           }}
         >
           <div className="col-md-6">Debit</div>
@@ -228,7 +231,7 @@ const TrialBalance = () => {
         </div>
         <hr />
       </div>
-      <div className="w-100" style={{ overflowY: 'auto' }}>
+      <div className="w-100" style={{ overflowY: "auto" }}>
         {newCoa && <BalanceLists list={assetsFill} />}
         {/* {trial && (
         <ReportList
@@ -243,7 +246,7 @@ const TrialBalance = () => {
       )} */}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default TrialBalance
+export default TrialBalance;
